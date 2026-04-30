44edb5fd82c389ef28948efa31f5ed50c0b3b215
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2026-04-30 12:57:40 +0300

    fix check output folder exists

--- app/src/main/java/com/foobnix/ext/EpubExtractor.java
+++ app/src/main/java/com/foobnix/ext/EpubExtractor.java
@@ -43,6 +43,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -87,7 +88,16 @@ public class EpubExtractor extends BaseExtractor {
 
         ArchiveEntry nextEntry = null;
 
-        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(new File(output)));
+        File outputFile = new File(output);
+
+        try {
+            Objects.requireNonNull(outputFile.getParentFile())
+                   .mkdirs();
+        }catch (Exception ignored){
+
+        }
+
+        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(outputFile));
         zos.setLevel(0);
 
         HypenUtils.applyLanguage(AppSP.get().hypenLang);
